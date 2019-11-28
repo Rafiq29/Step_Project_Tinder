00de968a5418722865f977eb65e659f999171695
temet_nosce <akifzade.ferid1995@gmail.com>
2019-11-28 11:10:40 +0400

    -- optimized

--- src/main/java/servlet/TestServlet.java
+++ src/main/java/servlet/TestServlet.java
@@ -9,7 +9,7 @@ import java.io.PrintWriter;
 
 public class TestServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter writer = resp.getWriter();
         writer.println("Rafiq the best");
         writer.close();
