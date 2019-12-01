52894a4e9c9f63ed35342b999ccdfeb22d873a54
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-12-01 19:19:02 +0400

    Added simple servlets without implementations

--- src/main/java/TinderApp.java
+++ src/main/java/TinderApp.java
@@ -1,17 +1,14 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlet.LikedServlet;
-import servlet.LoginServlet;
-import servlet.MessagesServlet;
-import servlet.TestServlet;
+import servlet.*;
 
 public class TinderApp {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8088);
 
         ServletContextHandler handler = new ServletContextHandler();
-        handler.addServlet(new ServletHolder(new TestServlet()),"/users/*");
+        handler.addServlet(new ServletHolder(new UserServlet()),"/users/*");
         handler.addServlet(new ServletHolder(new LoginServlet()),"/login/*");
         handler.addServlet(new ServletHolder(new MessagesServlet()),"/messages/*");
         handler.addServlet(new ServletHolder(new LikedServlet()),"/liked");
--- src/main/java/servlet/LikedServlet.java
+++ src/main/java/servlet/LikedServlet.java
@@ -1,6 +1,28 @@
 package servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class LikedServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Path path = Paths.get("content/people-list.html");
+        ServletOutputStream servletOutputStream =resp.getOutputStream();
+        Files.copy(path,servletOutputStream);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //After the user had clicked on all the available profiles
+        RedirectServlet rs = new RedirectServlet("/liked");
+    }
+
+
 }
