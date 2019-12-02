fddc8350e966eeb1470fd6881ffc436a4191984e
temet_nosce <akifzade.ferid1995@gmail.com>
2019-12-02 20:49:33 +0400

    -- message and users implemented

--- src/main/java/libs/User.java
+++ src/main/java/libs/User.java
@@ -1,4 +1,27 @@
 package libs;
 
 public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
