8bce1f29552216b2587c6603ced3c46930d4a1af
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-12-02 20:25:56 +0400

    Added simple servlets without implementations

--- src/main/java/TinderApp.java
+++ src/main/java/TinderApp.java
@@ -1,18 +1,19 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.log.*;
 import servlet.*;
 
 public class TinderApp {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8088);
-
         ServletContextHandler handler = new ServletContextHandler();
-        handler.addServlet(new ServletHolder(new MenuServlet()),"/");
+
         handler.addServlet(new ServletHolder(new UsersServlet()),"/users/*");
         handler.addServlet(new ServletHolder(new LoginServlet()),"/login/*");
         handler.addServlet(new ServletHolder(new MessagesServlet()),"/messages/*");
         handler.addServlet(new ServletHolder(new LikedServlet()),"/liked");
+        handler.addServlet(new ServletHolder(new MenuServlet()),"/*");
 
         server.setHandler(handler);
         server.start();
--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,6 +21,16 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        PrintWriter writer = resp.getWriter();
+        String login = req.getParameter("email_adress");
+        String password = req.getParameter("Password");
+        try {
+            writer.println("<p>Email_Adress: " + login + "</p>");
+            writer.println("<p>password: " + password + "</p>");
+        } finally {
+            writer.close();
+        }
     }
+        //Save(login,password);
+
 }
