988484c61621eb5c5b4793d52aea84797ad9da1f
vafamustafayeva <vafamustafayeva4@gmail.com>
2019-12-02 19:32:55 +0400

    -interface

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -3,7 +3,6 @@ package dao;
 import java.util.List;
 
 public interface DAO <T>{
-    void add(T user);
-    List<T> getAll();
-    List<T> getLiked();
+    void read(T user);
+    List<T> getDatabase();
 }
