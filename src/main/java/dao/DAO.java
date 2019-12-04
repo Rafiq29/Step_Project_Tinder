d6cdf7e97ef2745b0a059cda7914947c0dd99592
vafamustafayeva <vafamustafayeva4@gmail.com>
2019-12-04 21:46:59 +0400

    -database selected

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -1,10 +1,11 @@
 package dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO <T> extends Iterable<T> {
-    void read();
+    void read() throws SQLException;
     List<T> getDatabase();
     T getByValue(int id);
-    List<T> getAllId();
+    List<Integer> getAllId();
 }
--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -4,15 +4,28 @@ import libs.DbConnection;
 import libs.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
 
 public class UserDAO implements DAO<User> {
     private List<User> users;
-
+//TODO: Fix users.add
     @Override
-    public void read() {
+    public void read() throws SQLException {
         Connection conn = DbConnection.getConnection();
+        final String SQLQ = "SELECT*FROM users";
+        PreparedStatement preparedStatement = conn.prepareStatement(SQLQ);
+        ResultSet resultSet = preparedStatement.getResultSet();
+        while (resultSet.next())
+        {
+            String username = resultSet.getString("username");
+            String password = resultSet.getString("password");
+            int id = resultSet.getInt("id");
+            users.add(new User(id,username,password));
+        }
 
     }
 
@@ -23,6 +36,11 @@ public class UserDAO implements DAO<User> {
 
     @Override
     public User getByValue(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Integer> getAllId() {
         return null;
     }
 
