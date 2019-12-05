e52bb234b0188cffa481f216d17d6cb0062b1ab6
temet_nosce <akifzade.ferid1995@gmail.com>
2019-12-05 17:18:18 +0400

    -- updated

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -1,11 +1,10 @@
 package dao;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO <T> extends Iterable<T> {
-    void read() throws SQLException;
+    void read();
     List<T> getDatabase();
     T getByValue(int id);
-    List<Integer> getAllId();
+    List<T> getAllId();
 }
