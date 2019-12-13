d41861105d2379c870edf6d446af5b7c56af6491
temet_nosce <akifzade.ferid1995@gmail.com>
2019-12-13 16:30:53 +0400

    -- bugs fixed

--- src/main/java/TinderApp.java
+++ src/main/java/TinderApp.java
@@ -1,23 +1,17 @@
-import filters.EmptyCookieFilter;
-import filters.MessageFilter;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlet.*;
-
-import javax.servlet.DispatcherType;
-import java.util.EnumSet;
 
 public class TinderApp {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8088);
         ServletContextHandler handler = new ServletContextHandler();
 
-        handler.addFilter(new FilterHolder(new EmptyCookieFilter("/like/*")), "/like/*", EnumSet.of(DispatcherType.REQUEST));
-        handler.addFilter(new FilterHolder(new EmptyCookieFilter("/messages/*")), "/messages/*", EnumSet.of(DispatcherType.REQUEST));
-        handler.addFilter(new FilterHolder(new MessageFilter()), "/messages/*", EnumSet.of(DispatcherType.REQUEST));
-        handler.addFilter(new FilterHolder(new EmptyCookieFilter("/login/*")), "/login/*", EnumSet.of(DispatcherType.REQUEST));
+//        handler.addFilter(new FilterHolder(new EmptyCookieFilter("/like/*")), "/like/*", EnumSet.of(DispatcherType.REQUEST));
+//        handler.addFilter(new FilterHolder(new EmptyCookieFilter("/messages/*")), "/messages/*", EnumSet.of(DispatcherType.REQUEST));
+//        handler.addFilter(new FilterHolder(new MessageFilter()), "/messages/*", EnumSet.of(DispatcherType.REQUEST));
+//        handler.addFilter(new FilterHolder(new EmptyCookieFilter("/login/*")), "/login/*", EnumSet.of(DispatcherType.REQUEST));
 
         handler.addServlet(new ServletHolder(new RegisterServlet()), "/register/*");
         handler.addServlet(new ServletHolder(new LikeServlet()), "/like/*");
